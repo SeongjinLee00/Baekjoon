e94c8134e46b516ebcf5cf55347da70d3fc304b0
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-10 23:29:47 +0900

    [Silver II] Title: 최대공약수, Time: 124 ms, Memory: 133080 KB -BaekjoonHub

---
+++ 백준/Silver/1850.최대공약수/최대공약수.py
@@ -0,0 +1,21 @@
+def gcd(num1,num2): # Euclidian 
+    if num1>num2:
+        a=num1
+        b=num2
+    else:
+        a=num2
+        b=num1
+    r=a%b
+    if a%b==0:
+        return b
+    else:
+        return gcd(b,r)
+
+def lcm(num1,num2):
+
+    return num1*num2//gcd(num1,num2)
+
+A,B=map(int,input().split())
+C=gcd(A,B)
+D=gcd(A,C)
+print('1'*D)
