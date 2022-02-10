e2c98f0cbde64ad526895e232135fb99d1a1778a
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-10 23:45:51 +0900

    [Silver III] Title: 최소공배수, Time: 104 ms, Memory: 123316 KB -BaekjoonHub

---
+++ 백준/Silver/13241.최소공배수/최소공배수.py
@@ -0,0 +1,20 @@
+def gcd(num1,num2): # Euclidian 
+    if num1>num2:
+        a=num1
+        b=num2
+    else:
+        a=num2
+        b=num1
+    r=a%b
+    if a%b==0:
+        return b
+    else:
+        return gcd(b,r)
+
+def lcm(num1,num2):
+
+    return num1*num2//gcd(num1,num2)
+
+A,B=map(int,input().split())
+
+print(lcm(A,B))
