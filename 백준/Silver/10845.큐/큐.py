0f58e0ec7f4aa5124fb36ac23e83483ff3c5ce7e
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-13 01:24:05 +0900

    [Silver IV] Title: 큐, Time: 212 ms, Memory: 130324 KB -BaekjoonHub

---
+++ 백준/Silver/10845.큐/큐.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+import sys
+stack=deque([])
+
+N=int(sys.stdin.readline())
+
+for _ in range(N):
+    s=sys.stdin.readline().split()
+    if s[0]=='push':
+        stack.append(s[1])
+    if s[0]=='pop':
+        try:
+            print(stack.popleft())
+        except:
+            print(-1)
+    if s[0]=='size':
+        print(len(stack))
+    if s[0]=='empty':
+        try:
+            stack[0] # 맨앞 원소를 호출해서 인덱스에러나면 1출력, 안나면 0출력
+            print(0)
+        except:
+            print(1)
+    if s[0]=='front':
+        try:
+            print(stack[0])
+        except:
+            print(-1)
+    if s[0]=='back':
+        try:
+            print(stack[-1])
+        except:
+            print(-1)
