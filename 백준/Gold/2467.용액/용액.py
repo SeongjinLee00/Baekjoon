0bfd3786cd120f1f1116441bdc916ceef3b90452
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-12 17:43:56 +0900

    [Gold V] Title: 용액, Time: 148 ms, Memory: 140080 KB -BaekjoonHub

---
+++ 백준/Gold/2467.용액/용액.py
@@ -0,0 +1,24 @@
+N=int(input())
+numbers=list(map(int,input().split()))
+
+i=0
+j=N-1
+
+value=9999999999999
+sol1=0
+sol2=0
+
+while i<j:
+    if abs(numbers[i]+numbers[j])<value:
+        value=abs(numbers[i]+numbers[j])
+        sol1=numbers[i]
+        sol2=numbers[j]
+    
+    if numbers[i]+numbers[j]<0:
+        i+=1
+    elif numbers[i]+numbers[j]>0:
+        j-=1
+    else:
+        break
+
+print(sol1, sol2)
