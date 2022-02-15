bd77dc9b8ba8cd5c74f13bae359e579a523b59bf
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-15 17:17:47 +0900

    [Silver III] Title: 나무 자르기, Time: 632 ms, Memory: 284124 KB -BaekjoonHub

---
+++ 백준/Silver/2805.나무 자르기/나무 자르기.py
@@ -0,0 +1,29 @@
+N,M=map(int,input().split())
+
+trees=list(map(int,input().split()))
+
+start=0
+end=max(trees)
+
+while start<=end: # start가 더 커져야 탈출
+    cut=(start+end)//2 
+    harvest=sum([max(x-cut,0) for x in trees])
+    if harvest<M:
+        end=cut-1
+    elif harvest>M:
+        start=cut+1
+    else:
+        break
+
+if harvest==M:
+    print(cut)
+else:
+    for i in range(N+1):
+        harvest=sum([max(x-(cut+i),0) for x in trees])
+        if harvest>=M:
+            continue
+        if harvest<M:
+            cut=cut+i-1
+            break
+
+    print(cut)
