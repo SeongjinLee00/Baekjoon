b944a39d9232532d3ebf092f4a1287fe6764ad82
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-16 23:54:46 +0900

    [Silver III] Title: 랜선 자르기, Time: 172 ms, Memory: 127264 KB -BaekjoonHub

---
+++ 백준/Silver/1654.랜선 자르기/랜선 자르기.py
@@ -0,0 +1,24 @@
+K,N=map(int,input().split())
+
+wires=[]
+
+for _ in range(K):
+    wires.append(int(input()))
+
+start=1
+end=max(wires)
+
+while start<=end:
+    cut=(start+end)//2
+    get=sum([length//cut for length in wires])
+
+    if get>=N:
+        start=cut+1
+    elif get<N:
+        end=cut-1
+
+
+for k in range(2,-3,-1):
+    if sum([length//(cut+k) for length in wires])>=N:
+        print(cut+k)
+        break
