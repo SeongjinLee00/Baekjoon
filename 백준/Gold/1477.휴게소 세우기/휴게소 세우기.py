cde07aabd84b3ad2efd897d91fcc7c39f0f4ac49
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-17 13:46:35 +0900

    [Gold IV] Title: 휴게소 세우기, Time: 100 ms, Memory: 123316 KB -BaekjoonHub

--- 백준/Gold/1477.휴게소 세우기/휴게소 세우기.py
+++ 백준/Gold/1477.휴게소 세우기/휴게소 세우기.py
@@ -13,9 +13,9 @@ while start+1<end:
     for i in range(len(stations)-1): # 모든 구간을 d보다 작거나 같게 만드는 방법으로 휴게소를 지음
         cnt+=(stations[i+1]-stations[i]-1)//d
 
-    if cnt>M:
+    if cnt>M: # 그렇게 했는데 M개 초과짓는다? 구간을 늘려
         start=d
-    else:
+    else: # M보다 작거나 같다? 구간을 줄여
         end=d
 
 print(end)
