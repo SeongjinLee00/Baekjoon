6025993897781f38bb0e43838a54b5ebc917e655
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-02-21 00:33:16 +0900

    [Silver I] Title: 점프왕 쩰리 (Large), Time: 136 ms, Memory: 127828 KB -BaekjoonHub

---
+++ 백준/Silver/16174.점프왕 쩰리 （Large）/점프왕 쩰리 （Large）.py
@@ -0,0 +1,25 @@
+N=int(input())
+board=[]
+
+for _ in range(N):
+    board.append(list(map(int,input().split())))
+
+dr=[1,0]
+dc=[0,1]
+
+visited=[[0]*N for _ in range(N)]
+
+def dfs(r,c):
+    visited[r][c]=1
+    if board[r][c]==-1:
+        print('HaruHaru')
+        exit(0)
+    for i in range(2):
+        rr=r+dr[i]*board[r][c]
+        cc=c+dc[i]*board[r][c]
+        if 0<=rr<N and 0<=cc<N and board[r][c]!=0 and not visited[rr][cc]:
+            dfs(rr,cc)
+
+
+dfs(0,0)
+print('Hing')
