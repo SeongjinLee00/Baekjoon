34a13c5b698240514e7e4765a1a11a33aa4475d8
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-03-12 02:13:02 +0900

    [Silver V] Title: 다리 놓기, Time: 172 ms, Memory: 126532 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -3,14 +3,15 @@ T=int(input())
 for _ in range(T):
 
     N,M=map(int,input().split())
+    dp=[[0]*(M+1) for _ in range(N+1)]
 
-    head=1
-    foot=1
+    for i in range(1,N+1):
+        for j in range(1,M+1):
+            if i==1:
+                dp[i][j]=j
+            elif i==j:
+                dp[i][j]=1
+            else:
+                dp[i][j]=dp[i-1][j-1]+dp[i][j-1]
 
-    for i in range(M,M-N,-1):
-        head *= i
-
-    for j in range(1,N+1):
-        foot *= j
-
-    print(head//foot)
+    print(dp[N][M])
