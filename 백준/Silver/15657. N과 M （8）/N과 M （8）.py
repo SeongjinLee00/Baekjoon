48c6e342e1d0255f6ab07ce6e3ab16f1303214f5
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-03-19 18:34:14 +0900

    [Silver III] Title: N과 M (8), Time: 176 ms, Memory: 128000 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+N,M=map(int,input().split())
+
+numbers=list(map(int,input().split()))
+
+numbers.sort()
+
+def backtrack(arr,idx):
+
+    if len(arr)==M:
+        print(*arr)
+        return
+
+    for i in range(idx,N):
+        
+        arr.append(numbers[i])
+        backtrack(arr,i)
+        arr.pop()
+
+backtrack([],0)
