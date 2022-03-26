13cad15750a938622aca8b69825c6449169cce93
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-03-26 16:07:40 +0900

    [Gold III] Title: 저울, Time: 240 ms, Memory: 127692 KB -BaekjoonHub

---
+++ 백준/Gold/10159. 저울/저울.py
@@ -0,0 +1,30 @@
+N=int(input())
+M=int(input())
+
+costs=[[float('inf')]*N for _ in range(N)]
+for _ in range(M):
+    A,B=map(int,input().split())
+
+    costs[A-1][B-1]=1
+
+for i in range(N):
+    costs[i][i]=0
+
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            costs[i][j]=min(costs[i][j],costs[i][k]+costs[k][j])
+
+ans=0
+for i in range(N):
+    impossible=0
+    lighter=0
+    heavier=0
+    for j in range(N):
+        if costs[i][j]==float('inf') and costs[j][i]==float('inf'):
+            impossible+=1
+        elif costs[i][j]==float('inf') and costs[j][i]<float('inf'):
+            lighter+=1
+        elif costs[i][j]<float('inf') and costs[j][i]==float('inf'):
+            heavier+=1
+    print(impossible)
