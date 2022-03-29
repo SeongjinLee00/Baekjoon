c843141163f8683857d030f111ad456b255821a5
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-03-30 02:51:06 +0900

    [Gold V] Title: 최소비용 구하기, Time: 524 ms, Memory: 147312 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,35 @@
+import heapq
+import sys
+
+V=int(input())
+E=int(input())
+
+graph=[[]*(V+1) for _ in range(V+1)]
+distances=[float('inf')]*(V+1)
+for _ in range(E):
+    u,v,w=map(int,sys.stdin.readline().split())
+    graph[u].append([w,v])
+
+def Dijkstra(start):
+    distances[start]=0
+    q=[]
+    heapq.heappush(q,[distances[start],start])
+
+    while q:
+        curr_travel, curr_location=heapq.heappop(q)
+        
+        if distances[curr_location]<curr_travel: # 우선순위 큐를 쓰니까, 앞에 긴 경로 빠지기 전에 뒤에 짧은경로가 힙으로 들어올 수 있음
+            continue
+
+        for new_distance, new_destination in graph[curr_location]:
+
+            distance=curr_travel+new_distance
+            if distance<distances[new_destination]:
+                distances[new_destination]=distance
+                heapq.heappush(q,[distance,new_destination])
+
+start,end=map(int,input().split())
+
+Dijkstra(start)
+
+print(distances[end])
