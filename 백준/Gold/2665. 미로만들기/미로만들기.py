880399d533b4f11377ef58235d8e15f3ed576978
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-03-30 03:25:25 +0900

    [Gold IV] Title: 미로만들기, Time: 168 ms, Memory: 128176 KB -BaekjoonHub

---
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -0,0 +1,49 @@
+import heapq
+import sys
+
+N=int(input())
+if N==0:
+    exit(0)
+
+costs=[]
+for _ in range(N):
+    costs.append(list(map(int,list(input()))))
+
+V=N*N
+graph=[[]*(V+1) for _ in range(V+1)]
+distances=[float('inf')]*(V+1)
+
+for i in range(N):
+    for j in range(N):
+        node=i*N+j+1
+
+        if 0<=i+1<N:
+            graph[node].append([1-costs[i+1][j],node+N])
+        if 0<=i-1<N:
+            graph[node].append([1-costs[i-1][j],node-N])
+        if 0<=j+1<N:
+            graph[node].append([1-costs[i][j+1],node+1])
+        if 0<=j-1<N:
+            graph[node].append([1-costs[i][j-1],node-1])
+
+def Dijkstra(start):
+    distances[start]=1-costs[0][0]
+    q=[]
+    heapq.heappush(q,[distances[start],start])
+
+    while q:
+        curr_travel, curr_location=heapq.heappop(q)
+        
+        if distances[curr_location]<curr_travel: # 우선순위 큐를 쓰니까, 앞에 긴 경로 빠지기 전에 뒤에 짧은경로가 힙으로 들어올 수 있음
+            continue
+
+        for new_distance, new_destination in graph[curr_location]:
+
+            distance=curr_travel+new_distance
+            if distance<distances[new_destination]:
+                distances[new_destination]=distance
+                heapq.heappush(q,[distance,new_destination])
+
+Dijkstra(1)
+
+print(distances[V])
