ee6bdcfe3f8698eee2a44192acfd73d2883f9245
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-04-03 20:44:29 +0900

    [Gold IV] Title: 사이클 게임, Time: 432 ms, Memory: 129564 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -7,10 +7,10 @@ parent=[i for i in range(n)]
 def union(x,y):
     parent[find(y)]=find(x)
 
-def find(x):
-    while x!=parent[x]:
-        x=parent[x]
-    return x
+# def find(x):
+#     while x!=parent[x]:
+#         x=parent[x]
+#     return x
 
 # def find(x):
 #     if parent[x] != x:
@@ -26,12 +26,12 @@ def find(x):
 #         parent[n] = x
 #     return x
 
-# def find(x):
-#     tmp = x
-#     while x != parent[x]:
-#         x = parent[x]
-#     parent[tmp] = x
-#     return x
+def find(x):
+    tmp = x
+    while x != parent[x]:
+        x = parent[x]
+    parent[tmp] = x
+    return x
 
 for turn in range(1,m+1):
     a,b=map(int,sys.stdin.readline().split())
