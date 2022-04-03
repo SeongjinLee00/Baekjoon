0e1f6b913f3e119480a17b489ba44d13e68fd8c5
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-04-03 20:53:42 +0900

    [Gold IV] Title: 사이클 게임, Time: 384 ms, Memory: 386592 KB -BaekjoonHub

--- 백준/Gold/20040. 사이클 게임/사이클 게임.py
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -1,37 +1,21 @@
 import sys
+sys.setrecursionlimit(200000)
 
 n,m=map(int,input().split())
 
 parent=[i for i in range(n)]
 
-def union(x,y):
-    parent[find(y)]=find(x)
-
-# def find(x):
-#     while x!=parent[x]:
-#         x=parent[x]
-#     return x
-
-# def find(x):
-#     if parent[x] != x:
-#         parent[x] = find(parent[x])
-#     return parent[x]
-
-# def find(x):
-#     tmp = [x]
-#     while x != parent[x]:
-#         x = parent[x]
-#         tmp.append(x)
-#     for n in tmp:
-#         parent[n] = x
-#     return x
-
 def find(x):
-    tmp = x
-    while x != parent[x]:
-        x = parent[x]
-    parent[tmp] = x
-    return x
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x,y):
+    a=find(x)
+    b=find(y)
+
+    parent[max(a,b)]=min(a,b)
+
 
 for turn in range(1,m+1):
     a,b=map(int,sys.stdin.readline().split())
