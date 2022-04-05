fb3d1561f9e02304b8220c0e8b5cd62946ede1cc
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-04-05 09:59:21 +0900

    [Gold IV] Title: 별자리 만들기, Time: 124 ms, Memory: 125416 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
@@ -0,0 +1,39 @@
+def union(x,y):
+    a=find(x)
+    b=find(y)
+
+    parent[max(a,b)]=min(a,b)
+
+def find(x):
+    if parent[x] != x:
+        parent[x] = find(parent[x])
+
+    return parent[x]
+
+N=int(input())
+
+stars=[0]
+
+parent=[i for i in range(N+1)]
+
+graph=[]
+
+for _ in range(N):
+    x,y=map(float,input().split())
+    stars.append([x,y])
+
+for i in range(1,len(stars)-1):
+    for j in range(i,len(stars)):
+        distance=((stars[i][0]-stars[j][0])**2+(stars[i][1]-stars[j][1])**2)**0.5
+
+        graph.append([i,j,distance])
+
+graph.sort(key=lambda x:x[2])
+
+ans=0
+for start,end,cost in graph:
+    if find(start)!=find(end):
+        union(start,end)
+        ans+=cost
+
+print(ans)
