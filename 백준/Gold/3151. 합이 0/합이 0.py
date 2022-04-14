0e0ea1a5ba1d573f4d7481f8fb0db9fab9f9753b
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-04-15 02:55:23 +0900

    [Gold IV] Title: 합이 0, Time: 3272 ms, Memory: 115552 KB -BaekjoonHub

---
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -0,0 +1,28 @@
+from bisect import bisect_left
+
+N=int(input())
+
+numbers=list(map(int,input().split()))
+
+numbers.sort()
+
+ans=0
+for k in range(N-2):
+    i=k+1
+    j=N-1
+    
+    while i<j:
+        partialsum=numbers[k]+numbers[i]+numbers[j]
+        if partialsum==0:
+            if numbers[i]==numbers[j]:
+                ans+=j-i
+            else:
+                idx=bisect_left(numbers, numbers[j])
+                ans+=j-idx+1
+            i+=1
+        elif partialsum<0:
+            i+=1
+        elif partialsum>0:
+            j-=1
+
+print(ans)
