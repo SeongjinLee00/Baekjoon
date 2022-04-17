5872e2eade468da006a657ec96e0b6f58de444e3
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-04-18 03:19:03 +0900

    [Gold I] Title: 계단 수, Time: 276 ms, Memory: 131012 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -0,0 +1,16 @@
+N=int(input())
+
+dp=[[[0]*10 for _ in range(101)] for _ in range(1024)]
+
+for i in range(1, 10):
+    dp[1<<i][0][i] = 1
+
+for i in range(1, N):
+    for e in range(10):
+        for bm in range(1024):
+            if e < 9:
+                dp[bm|(1<<e)][i][e]=(dp[bm|(1<<e)][i][e]+dp[bm][i-1][e+1])
+            if e > 0:
+                dp[bm|(1<<e)][i][e]=(dp[bm|(1<<e)][i][e]+dp[bm][i-1][e-1])
+
+print(sum(dp[1023][N-1])%1000000000)
