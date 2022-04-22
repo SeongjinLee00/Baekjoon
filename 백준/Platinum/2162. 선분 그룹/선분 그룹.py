70d28d23c33950290d26cf025102966fcba49d9d
SeongjinLee00 <97649465+SeongjinLee00@users.noreply.github.com>
2022-04-22 15:44:15 +0900

    [Platinum V] Title: 선분 그룹, Time: 1568 ms, Memory: 133080 KB -BaekjoonHub

---
+++ 백준/Platinum/2162. 선분 그룹/선분 그룹.py
@@ -0,0 +1,73 @@
+def union(x,y):
+    parent[findset(y)]=findset(x)
+
+def findset(x):
+    if parent[x] != x:
+        parent[x] = findset(parent[x])
+    return parent[x]
+
+def is_CCW(vec1,vec2):
+    if vec1[0]*vec2[1]-vec2[0]*vec1[1]>0:
+        return 1
+    else :
+        return -1
+
+def is_inside(a,b,c):
+    if a<=c:
+        if a<=b<=c:
+            return True
+        else:
+            return False
+    if a>=c:
+        if a>=b>=c:
+            return True
+        else:
+            return False
+
+def is_intersect(x1,y1,x2,y2,x3,y3,x4,y4):
+    vector1=[x2-x1,y2-y1]
+    vector2=[x3-x2,y3-y2]
+    vector3=[x4-x2,y4-y2]
+
+    vector4=[x4-x3,y4-y3]
+    vector5=[x1-x4,y1-y4]
+    vector6=[x2-x4,y2-y4]
+
+    a=[x4-x1,y4-y1][0]*[x2-x4,y2-y4][1]-[x4-x1,y4-y1][1]*[x2-x4,y2-y4][0]
+    b=[x3-x1,y3-y1][0]*[x2-x3,y2-y3][1]-[x3-x1,y3-y1][1]*[x2-x3,y2-y3][0]
+    c=[x1-x3,y1-y3][0]*[x4-x1,y4-y1][1]-[x1-x3,y1-y3][1]*[x4-x1,y4-y1][0]
+    d=[x2-x3,y2-y3][0]*[x4-x2,y4-y2][1]-[x2-x3,y2-y3][1]*[x4-x2,y4-y2][0]
+
+    if [x1,y1]==[x3,y3] or [x1,y1]==[x4,y4] or [x2,y2]==[x3,y3] or [x2,y2]==[x4,y4]:
+        return 1
+
+    elif (a==0 and is_inside(x1,x4,x2) and is_inside(y1,y4,y2)) or (b==0 and is_inside(x1,x3,x2) and is_inside(y1,y3,y2)) or (c==0 and is_inside(x3,x1,x4) and is_inside(y3,y1,y4)) or (d==0 and is_inside(x3,x2,x4) and is_inside(y3,y2,y4)):
+        return 1
+
+    else:
+
+        if is_CCW(vector1,vector2)*is_CCW(vector1,vector3)==-1 and is_CCW(vector4,vector5)*is_CCW(vector4,vector6)==-1:
+            return 1
+        else:
+            return 0
+
+N=int(input())
+line_dict={}
+parent=[i for i in range(N+1)]
+
+for n in range(N):
+    x1,y1,x2,y2=map(int,input().split())
+    line_dict[(x1,y1,x2,y2)]=n+1
+
+    for line in line_dict:
+        if is_intersect(line[0],line[1],line[2],line[3],x1,y1,x2,y2):
+            union(line_dict[(x1,y1,x2,y2)],line_dict[line])
+
+ans=[0]*(N+1)
+for i in range(1,N+1):
+    findset(i)
+for i in range(1,N+1):
+    ans[i]=parent.count(i)
+
+print(len(ans)-ans.count(0))
+print(max(ans))
