b022b2e083882129f079bca61957b924804913ea
SeongjinLee00 <powerseongjin@gmail.com>
2022-07-02 21:45:42 +0900

    [Gold V] Title: 선 긋기, Time: 4932 ms, Memory: 249120 KB -BaekjoonHub

---
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -0,0 +1,31 @@
+import sys
+
+input=sys.stdin.readline
+
+N=int(input())
+
+ans=0
+
+lines=[]
+
+for _ in range(N):
+    s,e=map(int,input().split())
+    lines.append([s,e])
+
+lines.sort()
+
+ans=0
+S=lines[0][0]
+E=lines[0][1]
+for s,e in lines:
+    if S<=s and e<=E:
+        continue
+    elif s<=E:
+        E=e
+    else:
+        ans+=(E-S)
+        S=s
+        E=e
+
+ans+=(E-S)
+print(ans)
