b2feefd19d7752f420ede280aa3cda74624d6ef1
SeongjinLee00 <powerseongjin@gmail.com>
2022-07-16 20:26:48 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 488 ms, Memory: 132332 KB -BaekjoonHub

---
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -0,0 +1,45 @@
+import sys
+
+input=sys.stdin.readline
+
+N,M,K=map(int,input().split())
+
+tree=[0]*(2*N)
+
+for i in range(N):
+    tree[i+N]=int(input())
+
+for i in range(N-1,0,-1):
+    tree[i]=(tree[i*2]*tree[i*2+1])%1000000007
+
+def partialsum(s,e):
+    result=1
+    left=s+N-1
+    right=e+N-1
+    while left<=right:
+        if left%2:
+            result*=tree[left]
+            left+=1
+        if not right%2:
+            result*=tree[right]
+            right-=1
+        left//=2
+        right//=2
+        result%=1000000007
+    print(result%1000000007)
+
+def update(idx, num):
+    i=idx+N-1
+    tree[i]=num
+    i//=2
+    while i>=1:
+        tree[i]=(tree[i*2]*tree[i*2+1])%1000000007
+        i//=2
+
+for _ in range(M+K):
+    a,b,c=map(int,input().split())
+
+    if a==1:
+        update(b,c)
+    elif a==2:
+        partialsum(b,c)
