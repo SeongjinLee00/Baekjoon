d6ec02a71d381ae354a1ca5f9e67f5769a69d83c
SeongjinLee00 <powerseongjin@gmail.com>
2022-09-12 02:14:47 +0900

    [level 1] Title: 실패율, Time: 0.01 ms, Memory: 9.99 MB -BaekjoonHub

--- 프로그래머스/lv1/42889. 실패율/실패율.py
+++ 프로그래머스/lv1/42889. 실패율/실패율.py
@@ -1,26 +1,11 @@
-from collections import defaultdict
-
 def solution(N, stages):
-    challenge=defaultdict(int)
-    
-    for number in stages:
-        for ex in range(1,number+1):
-            challenge[ex]+=1
-    
-    ret=[]
-    for stage in range(1,N+1):
-        if challenge[stage]==0:
-            fail=0
+    result = {}
+    denominator = len(stages)
+    for stage in range(1, N+1):
+        if denominator != 0:
+            count = stages.count(stage)
+            result[stage] = count / denominator
+            denominator -= count
         else:
-            fail=(challenge[stage]-challenge[stage+1])/challenge[stage]
-
-        ret.append([fail,stage])
-        
-    ret.sort(key=lambda x:-x[0])
-    
-    ret2=[]
-    
-    for item in ret:
-        ret2.append(item[1])
-        
-    return ret2
+            result[stage] = 0
+    return sorted(result, key=lambda x : result[x], reverse=True)
