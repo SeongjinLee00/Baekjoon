f1402db45389d985dca441b9be1d66796ab88a0f
SeongjinLee00 <powerseongjin@gmail.com>
2022-09-23 23:14:31 +0900

    [Silver II] Title: N과 M (10), Time: 120 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,23 @@
+N,M=map(int,input().split())
+numbers=list(map(int,input().split()))
+numbers.sort()
+
+s=set()
+
+def backtrack(arr,m,last):
+    if m==M:
+        if tuple(arr) in s:
+            return
+        else:
+            print(*arr)
+            s.add(tuple(arr))
+            return
+
+    for i in range(N):
+
+        if i<=last:
+            continue
+
+        backtrack(arr+[numbers[i]],m+1,i)
+
+backtrack([],0,-1)
