a16e2f215f718a415bf263ec5ee4c718d2ab8024
SeongjinLee00 <powerseongjin@gmail.com>
2022-10-06 08:56:26 +0900

    [Bronze IV] Title: Winning Score, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/17009. Winning Score/Winning Score.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+e=int(input())
+f=int(input())
+
+A=3*a+2*b+c
+B=3*d+2*e+f
+
+if A>B:
+    print('A')
+elif A==B:
+    print('T')
+else:
+    print('B')
