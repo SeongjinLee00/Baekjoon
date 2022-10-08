912c0a9935406fd2ab7a95416e690f8e6a33b816
SeongjinLee00 <powerseongjin@gmail.com>
2022-10-08 09:31:59 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,4 +1,4 @@
 select o.animal_id, o.name from animal_outs o
-join animal_ins i on i.animal_id = o.animal_id
+inner join animal_ins i on i.animal_id = o.animal_id
 where o.datetime<i.datetime
 order by i.datetime
