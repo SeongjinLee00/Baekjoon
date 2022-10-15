78808d78000b57b0afa740f15c1cd8f955c378c1
SeongjinLee00 <powerseongjin@gmail.com>
2022-10-15 15:21:55 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 220 ms, Memory: 119464 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,51 +1,45 @@
-import heapq
-import sys
-
+from collections import deque
 t=0
 while True:
-    N=int(input())
-    if N==0:
-        exit(0)
-
-    costs=[]
-    for _ in range(N):
-        costs.append(list(map(int,input().split())))
-
-    V=N*N
-    graph=[[]*(V+1) for _ in range(V+1)]
-    distances=[float('inf')]*(V+1)
-
-    for i in range(N):
-        for j in range(N):
-            node=i*N+j+1
-
-            if 0<=i+1<N:
-                graph[node].append([costs[i+1][j],node+N])
-            if 0<=i-1<N:
-                graph[node].append([costs[i-1][j],node-N])
-            if 0<=j+1<N:
-                graph[node].append([costs[i][j+1],node+1])
-            if 0<=j-1<N:
-                graph[node].append([costs[i][j-1],node-1])
-
-    def Dijkstra(start):
-        distances[start]=costs[0][0]
-        q=[]
-        heapq.heappush(q,[distances[start],start])
-
-        while q:
-            curr_travel, curr_location=heapq.heappop(q)
-            
-            if distances[curr_location]<curr_travel: # 우선순위 큐를 쓰니까, 앞에 긴 경로 빠지기 전에 뒤에 짧은경로가 힙으로 들어올 수 있음
-                continue
-
-            for new_distance, new_destination in graph[curr_location]:
-
-                distance=curr_travel+new_distance
-                if distance<distances[new_destination]:
-                    distances[new_destination]=distance
-                    heapq.heappush(q,[distance,new_destination])
-
-    Dijkstra(1)
+ 
+    n=int(input())
     t+=1
-    print(f'Problem {t}: {distances[V]}')
+
+    if n==0:
+        exit(0)
+    a=[]
+    for i in range(n):
+        a.append(list(map(int,input().split())))
+ 
+    visited=[]
+    for i in range(n):
+        visited.append([999999 for i in range(n)])
+ 
+ 
+    dx=[0,0,-1,1]
+    dy=[1,-1,0,0]
+    def bfs(x,y):
+        queue=deque()
+        queue.append([x,y])
+ 
+        visited[x][y]=a[x][y]
+ 
+        while queue:
+            x,y = queue.popleft()
+ 
+            for i in range(4):
+                nx=x+dx[i]
+                ny=y+dy[i]
+ 
+                # if x==n-1 and y==n-1:
+                #     return visited[x][y]
+ 
+                if nx<0 or nx >=n or ny<0 or ny>=n:
+                    continue
+ 
+                if visited[nx][ny]> visited[x][y]+a[nx][ny]:
+                    visited[nx][ny]=visited[x][y]+a[nx][ny]
+                    queue.append([nx,ny])
+ 
+    bfs(0,0)
+    print(f'Problem {t}: {visited[n-1][n-1]}')
