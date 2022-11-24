89b49d263edf4cb8fb3ee1d7fb2140fcdc6edc88
SeongjinLee00 <powerseongjin@gmail.com>
2022-11-24 12:17:44 +0900

    [Bronze V] Title: Larger Sport Facility, Time: 304 ms, Memory: 115356 KB -BaekjoonHub

---
+++ 백준/Bronze/16099. Larger Sport Facility/Larger Sport Facility.py
@@ -0,0 +1,11 @@
+T=int(input())
+
+for _ in range(T):
+    a,b,c,d=map(int,input().split())
+    
+    if a*b>c*d:
+        print('TelecomParisTech')
+    elif a*b<c*d:
+        print('Eurecom')
+    else:
+        print('Tie')
