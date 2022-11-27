5171480ba9e458464e91ddd7e995a0b30ccb9153
SeongjinLee00 <powerseongjin@gmail.com>
2022-11-27 10:18:07 +0900

    [level 2] Title: N-Queen, Time: 1406.87 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12952. N－Queen/N－Queen.py
+++ 프로그래머스/lv2/12952. N－Queen/N－Queen.py
@@ -1,31 +1,15 @@
 def solution(n):
-    answer = 0
-    
-    def backtrack(rows,cols,queens):
-        nonlocal answer
-
-        if len(queens)==n:
-            answer+=1
-            return
-        
-        for next in range(len(queens)*n,len(queens)*n+n):
-            r=next//n
-            c=next%n
-            
-            if not rows[r] and not cols[c]:
-                for k in range(-r,0):
-                    if 0<=r+k<n and 0<=c+k<n and (r+k,c+k) in queens:
-                        break
-                    if 0<=r+k<n and 0<=c-k<n and (r+k,c-k) in queens:
-                        break
-                else:
-                    rows[r]=1
-                    cols[c]=1
-                    queens.add((r,c))
-                    backtrack(rows,cols,queens)
-                    rows[r]=0
-                    cols[c]=0
-                    queens.remove((r,c))
-    
-    backtrack([0]*n,[0]*n,set())
+    check_col = [False] * 100; check_d1 = [False] * 100; check_d2 = [False] * 100
+    def process(row):
+        answer = 0
+        if row == n+1:
+            return 1
+        for i in range(1,n+1):
+            d1 = row+i; d2 = n + (row - i)
+            if check_col[i] == False and check_d1[d1] == False and check_d2[d2] == False:
+                check_col[i] = True; check_d1[d1] = True; check_d2[d2] = True
+                answer += process(row+1)
+                check_col[i] = False; check_d1[d1] = False; check_d2[d2] = False
+        return answer
+    answer = process(1)
     return answer
