b57030d98975b0d9b89fbb3ba5d90551ffdfe5cf
SeongjinLee00 <powerseongjin@gmail.com>
2022-11-28 05:56:34 +0900

    [level 4] Title: 식품분류별 가장 비싼 식품의 정보 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
+++ 프로그래머스/lv4/131116. 식품분류별 가장 비싼 식품의 정보 조회하기/식품분류별 가장 비싼 식품의 정보 조회하기.sql
@@ -1,11 +1,16 @@
 -- 코드를 입력하세요
-# SELECT a.CATEGORY, a.PRICE AS MAX_PRICE, a.PRODUCT_NAME FROM FOOD_PRODUCT a
-# natural join (SELECT PRODUCT_NAME, MAX(PRICE) as m FROM FOOD_PRODUCT GROUP BY CATEGORY) b 
-# where a.CATEGORY IN ('과자', '국', '김치', '식용유')
-# ORDER BY MAX_PRICE DESC
+select a.category, a.price, a.product_name from food_product a
+join (select category, max(price) as MAX_PRICE from food_product group by category) b
+on a.category=b.category and a.price=b.MAX_PRICE
+having category in ('과자', '국', '김치', '식용유')
+order by price DESC
 
-SELECT CATEGORY, PRICE AS MAX_PRICE, PRODUCT_NAME
-  FROM FOOD_PRODUCT
- WHERE PRICE IN (SELECT MAX(PRICE) FROM FOOD_PRODUCT GROUP BY CATEGORY)
-   AND CATEGORY IN ('과자', '국', '김치', '식용유')
-ORDER BY MAX_PRICE DESC
+# SELECT FP.CATEGORY, SQ.MAX_PRICE, FP.PRODUCT_NAME
+# FROM FOOD_PRODUCT FP
+#      JOIN 
+#     (SELECT CATEGORY, MAX(PRICE) MAX_PRICE
+#      FROM FOOD_PRODUCT
+#      GROUP BY CATEGORY
+#      HAVING CATEGORY IN ('과자', '국', '김치', '식용유')) SQ
+#      ON FP.CATEGORY = SQ.CATEGORY AND FP.PRICE = SQ.MAX_PRICE 
+# ORDER BY SQ.MAX_PRICE DESC;
