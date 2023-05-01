6b4ab5d425bbee6635f81287b4cfea29af2d4e1a
SeongjinLee00 <powerseongjin@gmail.com>
2023-05-01 14:32:54 +0900

    [unrated] Title: 카운트 다운, Time: 0.05 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Solution {
+    public ArrayList solution(int start, int end) {
+        ArrayList answer = new ArrayList();
+        for (int num=start;num>=end;num--){
+            answer.add(num);
+        }
+        return answer;
+    }
+}
